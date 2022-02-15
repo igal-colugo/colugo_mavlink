6ccec9cd8df5ee937bdb5a3248f0fb9d5823e720
igal_colugo <igal@colugo-sys.com>
2022-02-15 14:07:30 +0200

    mav update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,90 +1,76 @@
-#!/usr/bin/env bash
 set -e
 
-SRC_DIR=$(pwd)
+SRC_DIR=`pwd`
 
-# NOTE: we must do all testing on the installed python package, not
+git submodule update --init --recursive
+
+# NOTE: we must do all testing on the installed python package, not 
 # on the build tree. Otherwise the testing is invalid and may not
 # indicate the code actually works
 
-test_format() {
-    # check format
-    sep="##############################################"
-    echo $sep
-    echo "FORMAT TEST"
-    echo $sep
-    cd "$SRC_DIR"
-    ./scripts/format_xml.sh -c
-    echo PASS
-}
+# check format
+sep="##############################################"
+echo $sep
+echo "FORMAT TEST"
+echo $sep
+cd $SRC_DIR
+./scripts/format_xml.sh -c
+echo PASS
 
-generate_mavlink() {
-    echo $sep
-    echo "GENERATING MAVLINK " \
-	    "protocol:${wire_protocol} language:${lang}"
-    echo "DEFINITION : " "$msg_def"
-    echo $sep
-    outdir="/tmp/mavlink_${wire_protocol}_${lang}"
-    pymavlink/tools/mavgen.py --lang="${lang}" \
-	    --wire-protocol "${wire_protocol}" \
-	    --strict-units \
-	    --output="${outdir}" "${msg_def}"
-    echo PASS
-}
+# install
+echo $sep
+echo "PYMAVLINK INSTALL"
+echo $sep
+cd $SRC_DIR
 
-test_py() {
-    cd "$SRC_DIR"
-    for msg_def in message_definitions/v1.0/*.xml
-    do
-        [ -e "$msg_def" ] || continue
-        wire_protocol="1.0"
-        for lang in Python C CS WLua Java
-        do
-            generate_mavlink
-        done
-        wire_protocol="2.0"
-        for lang in Python C C++11 CS WLua Java
-        do
-            generate_mavlink
-        done
-    done
-}
-
-test_node() {
-    # Avoid `spurious errors` caused by ~/.npm permission issues
-    # ref: https://github.com/travis-ci/travis-ci/issues/2244
-    # ref: https://github.com/npm/npm/issues/4815
-    # Does it already exist? Who owns? What permissions?
-    ls -lah ~/.npm || mkdir ~/.npm
-    # Make sure we own it
-    # $USER references the current user in Travis env
-    chown -R "$USER" ~/.npm
-    if [ -f /usr/bin/nodejs ]
-    then
-        mkdir -p ~/bin
-        ln -sf /usr/bin/nodejs ~/bin/node
-        . ~/.bashrc
-    fi
-    cd "$SRC_DIR/pymavlink/generator/javascript" && npm test
-}
-
-if [ "$#" -eq 1 ]; then
-    if [ "$1" == "format" ]; then
-        test_format
-    elif [ "$1" == "py" ]; then
-        test_py
-    elif [ "$1" == "node" ]; then
-        test_node
-    else
-        echo "Error: unknown argument '$1'"
-        echo ""
-        echo "Usage:"
-        echo "  $0 [py|node|format]"
-        exit 1
-    fi
-else
-    test_format
-    test_py
-    test_node
+user_arg="--user"
+if [ "$TRAVIS" == true ]
+then
+	user_arg=""
 fi
+pip install $user_arg -r pymavlink/requirements.txt
+cd $SRC_DIR/pymavlink
+python setup.py build install $user_arg
 
+msg_def="message_definitions/v1.0/common.xml"
+
+cd $SRC_DIR
+for wire_protocol in 1.0 2.0
+do
+	for lang in Python C CS WLua Java
+	do
+		echo $sep
+		echo "GENERATING MAVLINK " \
+			"protocol:${wire_protocol} language:${lang}"
+		echo $sep
+		outdir="/tmp/mavlink_${wire_protocol}_${lang}"
+		mavgen.py --lang=${lang} \
+			--wire-protocol ${wire_protocol} \
+			--output=${outdir} ${msg_def}
+		echo PASS
+	done
+done
+
+# Avoid `spurious errors` caused by ~/.npm permission issues
+# ref: https://github.com/travis-ci/travis-ci/issues/2244
+# ref: https://github.com/npm/npm/issues/4815
+# Does it already exist? Who owns? What permissions?
+ls -lah ~/.npm || mkdir ~/.npm
+# Make sure we own it
+# $USER references the current user in Travis env
+chown -R $USER ~/.npm
+if [ -f /usr/bin/nodejs ]
+then
+	mkdir -p ~/bin
+	ln -sf /usr/bin/nodejs ~/bin/node
+	. ~/.bashrc	
+fi
+cd $SRC_DIR/pymavlink/generator/javascript && npm test
+
+# Test quaternions
+echo $sep
+echo "QUATERNION TEST"
+echo $sep
+cd $SRC_DIR
+pymavlink/tools/quaterniontest.py
+echo PASS
